1235cf47329cf6b300af1c83b6004483d289122a
Edouard Menayde <edouard.menayde@gmail.com>
2018-11-04 17:07:34 +0100

    Second commit

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 extern crate core;
 extern crate hyper;
-//extern crate futures;
-//extern crate hyper_tls;
-//extern crate tokio;
+extern crate tokio;
+extern crate futures;
+extern crate hyper_tls;
 
 use std::rc::Rc;
 use std::cell::RefCell;
@@ -15,33 +15,40 @@ use std::thread;
 use core::time;
 
 use hyper::Uri;
-//use futures::{future, Future};
-//use hyper_tls::HttpsConnector;
-//use hyper::{Client, StatusCode, Response};
-//use hyper::rt::{self, Stream};
-//use std::io;
-//use std::io::Write;
-//use hyper::service::service_fn;
-//use hyper::Body;
-//use hyper::client::HttpConnector;
-//use std::borrow::BorrowMut;
+use tokio::runtime::Runtime;
+use tokio::prelude::future::FutureResult;
+use futures::{future, Future};
+use hyper::{Client, StatusCode, Response};
+use hyper_tls::HttpsConnector;
+use hyper::client::HttpConnector;
+use hyper::rt::{self};
+use std::result::Result::Ok;
+use futures::Poll;
+use tokio::prelude::IntoFuture;
+use futures::Stream;
+use tokio::io;
+use hyper::client::ResponseFuture;
 
-//fn fetch_uri(url: Uri, client: &Client<HttpsConnector<HttpConnector>>) -> Box<Future<Item = (), Error = ()> + Send + 'static> {
-//    Box::new(tokio::run(future::lazy(|| {
-//        client
-//            .get(url)
-//            .map(|res| {
-//                if res.status() == StatusCode::MOVED_PERMANENTLY {
-//                    println!("{}", res.headers().get("Location").unwrap().to_str().unwrap());
-//                } else {
-//                    println!("{}", res.status());
-//                }
-//            })
-//            .map(|_| {
-//                println!("\n\nFinished checking website.");
-//            })
-//            .map_err(|e| eprintln!("request error: {}", e))
-//    })))
+fn process_url(url: Uri, processing_units: Arc<AtomicUsize>, is_empty: Arc<Condvar>, client: Client<HttpsConnector<HttpConnector>>) -> impl Future<Item=(), Error=()> {
+    println!("Processed {}", url);
+    client
+        .get(url)
+        .then(move |res|{
+            let res = res.unwrap();
+            if res.status() == StatusCode::MOVED_PERMANENTLY {
+                println!("{}", res.headers().get("Location").unwrap().to_str().unwrap());
+            } else {
+                println!("{}", res.status());
+            }
+
+            println!("\n\nFinished checking website.");
+            processing_units.fetch_sub(1, Ordering::SeqCst);
+            is_empty.notify_one();
+
+            future::ok(())
+        })
+}
+
 fn main() {
     let url = match env::args().nth(1) {
         Some(url) => url,
@@ -55,19 +62,25 @@ fn main() {
 
     let links: Rc<RefCell<Vec<Uri>>> = Rc::new(RefCell::new(Vec::new()));
 
-    links.borrow_mut().push(url);
+    links.borrow_mut().push(url.clone());
 
     let mutex = Arc::new(Mutex::new(false));
     let is_empty = Arc::new(Condvar::new());
     let has_finished_checking = Arc::new(AtomicBool::new(false));
     let processing_units = Arc::new(AtomicUsize::new(0));
 
+    let mut rt = Runtime::new().unwrap();
+
+    let https = HttpsConnector::new(4).unwrap();
+    let client = Client::builder()
+        .build::<_, hyper::Body>(https);
+
     loop {
         println!("Looping");
         let link;
 
         {
-            let mut mutex_guard = mutex.lock().unwrap();
+            let mutex_guard = mutex.lock().unwrap();
 
             if processing_units.load(Ordering::SeqCst) == 0 && links.borrow().is_empty() {
                 has_finished_checking.store(true, Ordering::SeqCst);
@@ -80,7 +93,7 @@ fn main() {
 
             if links.borrow().is_empty() {
                 println!("Empty");
-                mutex_guard = is_empty.wait(mutex_guard).unwrap();
+                is_empty.wait(mutex_guard).unwrap();
             }
 
             if processing_units.load(Ordering::SeqCst) == 0 && links.borrow().is_empty() {
@@ -95,41 +108,12 @@ fn main() {
             link = links.borrow_mut().pop().unwrap();
         }
 
-        let process_url = |url| {
-            let processing_units = Arc::clone(&processing_units);
-            println!("Processed {}", url);
-            thread::sleep(time::Duration::from_millis(10));
-            processing_units.fetch_sub(1, Ordering::SeqCst);
-            is_empty.notify_one();
-        };
-
-
         {
-            let processing_units = Arc::clone(&processing_units);
             processing_units.fetch_add(1, Ordering::SeqCst);
-            process_url(link);
+            let processing_units = Arc::clone(&processing_units);
+            let is_empty = Arc::clone(&is_empty);
+            let client = client.clone();
+            rt.spawn(future::lazy(|| process_url(link, processing_units, is_empty, client)));
         }
     }
-
-//    tokio::run(future::lazy(|| {
-    // 4 is number of blocking DNS threads
-//    let https = HttpsConnector::new(4).unwrap();
-//    let client = Client::builder()
-//        .build::<_, hyper::Body>(https);
-//
-//
-//    let new_service = move |url| {
-//        let client = client.clone();
-//        service_fn(move |res| {
-//            fetch_uri(url, &client);
-//        })
-//    };
-
-//        while !has_finished_checking {
-//    new_service(url);
-//        }
-//    }));
 }
-
-
-//}
